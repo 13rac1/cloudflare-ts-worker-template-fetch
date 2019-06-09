60992fbceaa13e4f1128b5f583fa537e9867746b
Brad Erickson <bderickson@gmail.com>
2019-06-08 18:19:57 -0700

    feat: Convert to TypeScript
    Minimum possible number of characters changed from upstream.
    
    Multiple minor bug fixes to resolve undefined behavior.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Examples of making fetch requests from within your Worker script including generating JSON post requests then reading in the resulting response body, aggregating multiple requests into one response, and following/catching redirects.
 
-[`index.js`](https://github.com/cloudflare/worker-template-fetch/blob/master/index.js) is the content of the Workers script.
+[`index.ts`](https://github.com/13rac1/cloudflare-ts-worker-template-fetch/blob/master/index.ts) is the content of the Workers script.
 
 Live Demos are hosted on `workers-tooling.cf/demos/fetch`:
 [Demo JSON](http://workers-tooling.cf/demos/fetch/json) | [Demo HTML](http://workers-tooling.cf/demos/fetch/html)
@@ -11,8 +11,11 @@ Live Demos are hosted on `workers-tooling.cf/demos/fetch`:
 To generate using [wrangler](https://github.com/cloudflare/wrangler)
 
 ```
-wrangler generate myApp https://github.com/cloudflare/worker-template-fetch
+wrangler generate myApp https://github.com/13rac1/cloudflare-ts-worker-template-fetch
 ```
+
+#### Build
+Run `npm run build` to build with TypeScript
 
 #### Serverless
 To deploy using serverless add a [`serverless.yml`](https://serverless.com/framework/docs/providers/cloudflare/) file.
--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
+import CloudflareWorkerGlobalScope from 'types-cloudflare-worker'
+declare var self: CloudflareWorkerGlobalScope
+
 /**
  * Example someHost is set up to respond with JSON and HTML according to the path
- *  */
+ *
+ */
 const someHost = 'https://workers-tooling.cf/demos'
 const someJSONURL = someHost + '/requests/json'
 const someHTMLURL = someHost + '/static/html'
-const someJSONToSend = {
+
+interface JSONToSend {
+  results?: string[],
+  errors?: string | null,
+  msg?: string,
+}
+const someJSONToSend:JSONToSend = {
   results: ['default data to send'],
   errors: null,
   msg: 'I sent this to the fetch',
@@ -20,9 +30,9 @@ const someDefaultJSONToRespond = {
  * Use await gatherResponse(..) in an async function to get the response body
  * @param {Response} response to
  */
-async function gatherResponse(response) {
+async function gatherResponse(response: Response) {
   const { headers } = response
-  const contentType = headers.get('content-type')
+  const contentType = headers.get('content-type') || ""
 
   if (contentType.includes('application/json')) {
     const body = await response.json()
@@ -46,7 +56,7 @@ async function gatherResponse(response) {
  * @param {string} url the URL to send the request to
  * @param {BodyInit} body the JSON data to send in the request
  */
-async function fetchPostJson(url, body = {}) {
+async function fetchPostJson(url: string, body: JSONToSend = {}): Promise<string> {
   const init = {
     body: JSON.stringify(body),
     method: 'POST',
@@ -66,13 +76,7 @@ async function fetchPostJson(url, body = {}) {
  * Use await fetchGetHtml(..) in an async function to get the HTML
  * @param {string} url the URL to send the request to
  */
-async function fetchGetHtml(url) {
-  const init = {
-    method: 'Get',
-    headers: {
-      'content-type': 'text/html;charset=UTF-8',
-    },
-  }
+async function fetchGetHtml(url: string) {
 
   const response = await fetch(url)
   const respBody = await gatherResponse(response)
@@ -81,22 +85,15 @@ async function fetchGetHtml(url) {
 
 /**
  * Example of how fetch methods above can be used in an application
- *  */
-
-addEventListener('fetch', async event => {
-  const { url, method } = event.request
+ *
+ */
+self.addEventListener('fetch', (event: FetchEvent) => {
+  const { url } = event.request
+  let init: ResponseInit
+  let respBody: Promise<string>
 
   // Set respBody and init according to the route
   // and method of the incoming request
-  if (url.endsWith('/html')) {
-    init = {
-      headers: {
-        'content-type': 'text/html;charset=UTF-8',
-      },
-    }
-    respBody = fetchGetHtml(someHTMLURL)
-  }
-
   if (url.endsWith('/json')) {
     init = {
       headers: {
@@ -104,6 +101,15 @@ addEventListener('fetch', async event => {
       },
     }
     respBody = fetchPostJson(someJSONURL, someJSONToSend)
+  }
+  else { //url.endsWith('/html')
+    // Default to HTML
+    init = {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    }
+    respBody = fetchGetHtml(someHTMLURL)
   }
 
   // Turn the the respBody string into a Response
