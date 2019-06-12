625dd79cf20315dea0e48be8098aca4d5b5f2ff2
Brad Erickson <bderickson@gmail.com>
2019-06-12 16:33:01 -0700

    chore: Convert receive-server to TypeScript

--- receive-server.ts
+++ receive-server.ts
@@ -1,3 +1,6 @@
+import CloudflareWorkerGlobalScope from 'types-cloudflare-worker'
+declare var self: CloudflareWorkerGlobalScope
+
 const someHTML = `
 <!DOCTYPE html>
 <html>
@@ -17,8 +20,13 @@ const someHTML = `
 </body>
 </html>
 `
-const someJSON = {
-  result: ['some', 'results'],
+interface someJSON {
+  results?: string[]
+  errors?: string | null
+  msg?: string
+}
+const someJSON: someJSON = {
+  results: ['some', 'results'],
   errors: null,
   msg: 'this is some random json',
 }
@@ -28,7 +36,7 @@ const someJSON = {
  * into the worker script
  * @param {string} html
  */
-async function rawHtmlResponse(html) {
+async function rawHtmlResponse(html: string) {
   const init = {
     headers: {
       'content-type': 'text/html;charset=UTF-8',
@@ -43,14 +51,14 @@ async function rawHtmlResponse(html) {
  * into the worker script
  * @param {Object} json
  */
-async function rawJsonResponse(json) {
+async function rawJsonResponse(json: someJSON) {
   const init = {
     headers: {
       'content-type': 'application/json;charset=UTF-8',
     },
   }
 
-  return new Response(json, init)
+  return new Response(JSON.stringify(json), init)
 }
 
 //TODO add simple make subrequest
@@ -59,7 +67,7 @@ async function rawJsonResponse(json) {
  * TODO changeto use KV will need mime types https://www.npmjs.com/package/mime-types
  */
 
-addEventListener('fetch', event => {
+self.addEventListener('fetch', (event: FetchEvent) => {
   const { url } = event.request
 
   if (url.endsWith('/html')) {
